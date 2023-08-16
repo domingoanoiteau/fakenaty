237bfbf11d0905c54efe7a95e5547f79e1ca6b12
Rian Paiva <141244068+domingoanoiteau@users.noreply.github.com>
2023-08-16 10:17:07 -0300

    Add files via upload

---
+++ exfunction.js
@@ -0,0 +1,22 @@
+function soma(n1, n2){
+    return n1 + n2
+};
+soma(10,5)
+console.log(soma(10,5));
+
+function sub (n3, n4){
+    return n3 - n4
+};
+sub(45,5)
+console.log(sub(10,5));
+
+function mult(n5, n6){
+    return n5 * n6
+}; 
+mult(100,10)
+console.log(mult(100,10));
+
+function div(n7, n8){
+    return n7 / n8
+};
+console.log(div(50,25));
